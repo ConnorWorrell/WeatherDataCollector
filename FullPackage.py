2a6bd6a6588835d89d824f360a587c48ff4ed62a
BuildTools <unconfigured@null.spigotmc.org>
2018-08-13 17:12:25 -0600

    Added Comments

--- FullPackage.py
+++ FullPackage.py
@@ -5,15 +5,19 @@ import datetime
 import uszipcode
 import urllib.request
 
+#Open base workbook
 book = xlrd.open_workbook(r"C:\Users\Connor\Documents\TestAPIRequestBook.xlsx")
 sheetRead = book.sheet_by_index(0)
 
+#Open offline data workbook
 databook = xlrd.open_workbook(r"C:\Users\Connor\Documents\TestAPIRawData.xls")
 dataRead = databook.sheet_by_index(0)
 
+#Create output workbook
 workbook = xlwt.Workbook()
 sheetWrite = workbook.add_sheet('test')
 
+#Create output offline data workbook
 workbookData = xlwt.Workbook()
 dataWrite = workbookData.add_sheet('test')
 
@@ -22,48 +26,60 @@ print("Starting " + str(sheetRead.nrows-1) + " rows calculations")
 #Duplicate read workbook
 for i in range(sheetRead.nrows):
     for p in range(sheetRead.ncols):
-        if sheetRead.cell_value(i,p) != "":
+        if sheetRead.cell_value(i,p) != "": #If cell is empty don't copy any data since you can only write to each cell once
             sheetWrite.write(i, p, sheetRead.cell_value(i,p))
 
+#Duplicate offline data workbook
 for i in range(dataRead.nrows):
     for p in range(dataRead.ncols):
-        if dataRead.cell_value(i,p) != "":
+        if dataRead.cell_value(i,p) != "": #If cell is empty don't copy any data since you can only write to each cell once
             dataWrite.write(i, p, dataRead.cell_value(i,p))
 
+#Make an array of all the unix times stored in offline data workbook
 UnixTimeData = []
 for i in range(dataRead.nrows):
     UnixTimeData.append(dataRead.cell_value(i,0))
 
 print(UnixTimeData)
 
+#Define what search is
 search = uszipcode.ZipcodeSearchEngine()
 
+#Count number written to offline data workbook, used later to determine what line to write to in offline data workbook
 WrittenIterations = 0
 
+#Iterate through each row in base workbook
 for i in range(sheetRead.nrows-1):
     print("StartingRow" + str(i+1))
+    #Determine the longitude and lattitude and write them to the output workbook
     Longitude = search.by_zipcode(str(int(sheetRead.cell_value(i + 1, 0)))).Longitude
     Lattertude = search.by_zipcode(str(int(sheetRead.cell_value(i + 1, 0)))).Latitude
     sheetWrite.write(i + 1, 4, Longitude)
     sheetWrite.write(i + 1, 5, Lattertude)
 
+    #Get Day Month and Year from the workbook
     Day = str(int(sheetRead.cell_value(i+1,1)))
     Month = str(int(sheetRead.cell_value(i + 1, 2)))
     Year = str(int(sheetRead.cell_value(i + 1, 3)))
 
+    #Use time package to calculate Unix time
     UnixTime = time.mktime(datetime.datetime.strptime(Day+"/"+Month+"/"+Year, "%d/%m/%Y").timetuple())
 
+    #Write the unix time to the output workbook
     sheetWrite.write(i + 1, 6, UnixTime)
 
+    #If the unix time is in the offline data workbook use the data from the offline data workbook
     if(UnixTime in UnixTimeData):
         print("Data Found On Computer")
         contents = dataRead.cell_value(UnixTimeData.index(UnixTime),1)
 
+    #If the unix time isn't in the offline data workbook request it from the website
     else:
         contents = urllib.request.urlopen("https://api.darksky.net/forecast/8066119e9963349cca1c07b7b9740e45/" + str(Lattertude) + "," + str(Longitude) + "," + str(int(UnixTime))).read()
         print("Data Found Online")
         contents = contents.decode("utf-8")
 
+        #Write the new data that was found to the offline data workbook
         dataWrite.write(dataRead.nrows + WrittenIterations, 0, UnixTime)
         dataWrite.write(dataRead.nrows + WrittenIterations, 1, contents)
 
@@ -71,134 +87,177 @@ for i in range(sheetRead.nrows-1):
 
         workbookData.save(r"C:\Users\Connor\Documents\TestAPIRawData.xls")
 
+    #Filter data into into 2 arrays Titles containing names, and contentsplit containing numbers
     contentssplit = contents.split(",")
-
     Titles = contentssplit
-    # Filter data
+
+    #Remove all numbers from Titles, also remove summary before summary title
     for Char in '1234567890- [{}":.]':
         Titles = [s.replace(Char, '') for s in Titles]
     Titles = [s.replace('summary', '') for s in Titles]
 
+    #Remove all characters from content split
     RemoveChar = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQ[RSTUVWXYZ['{]}:/"
-
     for Char in RemoveChar:
         contentssplit = [s.replace(Char, '') for s in contentssplit]
 
+    #remove " characters from content split
     contentssplit = [s.replace('"', '') for s in contentssplit]
 
-    sheetWrite.write(i + 1, 7, Titles[4])   # Summary
+    #Write summary title to output sheet
+    sheetWrite.write(i + 1, 7, Titles[4])
 
+    #Try:
+        #Write to output sheet the first number that has the title of "Something"
+    #If there is a error
+        #Write nothing
+
+    #PrecipitationIntensityHigh
     try:
-        sheetWrite.write(i + 1, 8, contentssplit[Titles.index("precipIntensityMax")])   # PrecipIntensityHigh
+        sheetWrite.write(i + 1, 8, contentssplit[Titles.index("precipIntensityMax")])
     except ValueError:
-        sheetWrite.write(i + 1, 8, "")  # PrecipIntensityHigh
+        sheetWrite.write(i + 1, 8, "")
 
+    #PrecipitationProbabilityHigh
     try:
-        sheetWrite.write(i + 1, 9, contentssplit[Titles.index("precipProbability")])   # PrecipProbabilityHigh
+        sheetWrite.write(i + 1, 9, contentssplit[Titles.index("precipProbability")])
     except ValueError:
-        sheetWrite.write(i + 1, 9, "")  # PrecipProbabilityHigh
+        sheetWrite.write(i + 1, 9, "")
 
+    #TempratureHigh
     try:
-        sheetWrite.write(i + 1, 10, contentssplit[Titles.index("temperatureHigh")])   # TempratureHigh
+        sheetWrite.write(i + 1, 10, contentssplit[Titles.index("temperatureHigh")])
     except ValueError:
-        sheetWrite.write(i + 1, 10, "")  # TempratureHigh
+        sheetWrite.write(i + 1, 10, "")
 
+    #TempratureLow
     try:
-        sheetWrite.write(i + 1, 11, contentssplit[Titles.index("temperatureLow")])  # TempratureLow
+        sheetWrite.write(i + 1, 11, contentssplit[Titles.index("temperatureLow")])
     except ValueError:
-        sheetWrite.write(i + 1, 11, "")  # TempratureLow
+        sheetWrite.write(i + 1, 11, "")
 
+    #ApparentTempratureHigh
     try:
-        sheetWrite.write(i + 1, 12, contentssplit[Titles.index("apparentTemperatureHigh")])  # ApparentTempratureHigh
+        sheetWrite.write(i + 1, 12, contentssplit[Titles.index("apparentTemperatureHigh")])
     except ValueError:
-        sheetWrite.write(i + 1, 12, "")  # ApparentTempratureHigh
+        sheetWrite.write(i + 1, 12, "")
 
+    #ApparentRempratureLow
     try:
-        sheetWrite.write(i + 1, 13, contentssplit[Titles.index("apparentTemperatureLow")])  # ApparentTempratureLow
+        sheetWrite.write(i + 1, 13, contentssplit[Titles.index("apparentTemperatureLow")])
     except ValueError:
-        sheetWrite.write(i + 1, 13, "")  # ApparentTempratureLow
+        sheetWrite.write(i + 1, 13, "")
 
+    #DewPoint
     try:
-        sheetWrite.write(i + 1, 14, contentssplit[Titles.index("dewPoint")])  # DewPoint
+        sheetWrite.write(i + 1, 14, contentssplit[Titles.index("dewPoint")])
     except ValueError:
-        sheetWrite.write(i + 1, 14, contentssplit[Titles.index("dewPoint")])  # DewPoint
+        sheetWrite.write(i + 1, 14, "")
 
+    #Humidity
     try:
-        sheetWrite.write(i + 1, 15, contentssplit[Titles.index("humidity")])  # Humidity
+        sheetWrite.write(i + 1, 15, contentssplit[Titles.index("humidity")])
     except ValueError:
-        sheetWrite.write(i + 1, 15, contentssplit[Titles.index("humidity")])  # Humidity
+        sheetWrite.write(i + 1, 15, "")
 
+    #Pressure
     try:
-        sheetWrite.write(i + 1, 16, contentssplit[Titles.index("pressure")])  # Pressure
+        sheetWrite.write(i + 1, 16, contentssplit[Titles.index("pressure")])
 
+        #Find the indicies of all things titled pressure
         indices = [pp for pp, x in enumerate(Titles) if x == "pressure"]
+
+        #Initialize the lowest and highest to the first number in the list
         lowest = float(contentssplit[indices[0]])
         highest = float(contentssplit[indices[0]])
+
+        #Iterate through the indicies and replace highest if i2 is higher than highest and lower if i2 is lower than lowest
         for i2 in indices:
             if (highest < float(contentssplit[i2])):
                 highest = float(contentssplit[i2])
             if (lowest > float(contentssplit[i2])):
                 lowest = float(contentssplit[i2])
+
+        #Calculate the change and write it to output sheet
         Change = highest - lowest
-        sheetWrite.write(i + 1, 17, Change)  # PressureChange
+        sheetWrite.write(i + 1, 17, Change)
     except ValueError:
-        sheetWrite.write(i + 1, 16, "")  # Pressure
-        sheetWrite.write(i + 1, 17, "")  # PressureChange
+        sheetWrite.write(i + 1, 16, "")
+        sheetWrite.write(i + 1, 17, "")
 
+    #WindSpeed
     try:
-        sheetWrite.write(i + 1, 18, contentssplit[Titles.index("windSpeed")])  # WindSpeed
+        sheetWrite.write(i + 1, 18, contentssplit[Titles.index("windSpeed")])
     except ValueError:
-        sheetWrite.write(i + 1, 18, "")  # WindSpeed
+        sheetWrite.write(i + 1, 18, "")
 
+    #WindGust
     try:
-        sheetWrite.write(i + 1, 19, contentssplit[Titles.index("windGust")])  # WindGust
+        sheetWrite.write(i + 1, 19, contentssplit[Titles.index("windGust")])
     except ValueError:
-        sheetWrite.write(i + 1, 19, "")  # WindGust
+        sheetWrite.write(i + 1, 19, "")
 
+    #WindBearing
     try:
-        sheetWrite.write(i + 1, 20, contentssplit[Titles.index("windBearing")])  # WindBearing
+        sheetWrite.write(i + 1, 20, contentssplit[Titles.index("windBearing")])
     except ValueError:
-        sheetWrite.write(i + 1, 20, "")  # WindBearing
+        sheetWrite.write(i + 1, 20, "")
 
+    #CloudCover
     try:
-        sheetWrite.write(i + 1, 21, contentssplit[Titles.index("cloudCover")])  # CloudCover
+        sheetWrite.write(i + 1, 21, contentssplit[Titles.index("cloudCover")])
     except ValueError:
-        sheetWrite.write(i + 1, 21, "")  # CloudCover
+        sheetWrite.write(i + 1, 21, "")
 
-    try: #Some past things don't have uv or ozone numbers
+    #UVIndex
+    try: #Find the highest uv index
 
+        #Find indicies of things titled uvIndex
         indices = [pp for pp, x in enumerate(Titles) if x == "uvIndex"]
+
+        #If something was found titled uvindex
         if (len(indices) > 0):
+
+            #Initialize highest value to first value in indices
             UVHighest = float(contentssplit[indices[0]])
+
+            #Iterate through indices, replace highest if i2 is higher than highest
             for i2 in indices:
                 if (UVHighest < float(contentssplit[i2])):
                     UVHighest = float(contentssplit[i2])
-            sheetWrite.write(i + 1, 22, UVHighest)  # uvIndex
+
+            #Write to output workbook
+            sheetWrite.write(i + 1, 22, UVHighest)
         else:
             sheetWrite.write(i + 1, 22, "")
 
     except ValueError:
         sheetWrite.write(i + 1, 22, "")
 
+    #Visibility
     try:
-        sheetWrite.write(i + 1, 23, contentssplit[Titles.index("visibility")])  # Visibility
+        sheetWrite.write(i + 1, 23, contentssplit[Titles.index("visibility")])
     except ValueError:
-        sheetWrite.write(i + 1, 23, "")  # Visibility
+        sheetWrite.write(i + 1, 23, "")
 
+    #Ozone
     try:
-        sheetWrite.write(i + 1, 24, contentssplit[Titles.index("ozone")])  # Ozone
+        sheetWrite.write(i + 1, 24, contentssplit[Titles.index("ozone")])
     except ValueError:
         sheetWrite.write(i + 1, 24, "")
 
+    #Moon Phase
     try:
-        sheetWrite.write(i + 1, 25, contentssplit[Titles.index("moonPhase")])  # moonPhase
+        sheetWrite.write(i + 1, 25, contentssplit[Titles.index("moonPhase")])
     except ValueError:
-        sheetWrite.write(i + 1, 25, "")  # moonPhase
+        sheetWrite.write(i + 1, 25, "")
 
+    #Save output workbook after each line
     print("Saving")
     workbook.save(r"C:\Users\Connor\Documents\TextBookWrite.xls")
     print("Saved")
 
+#Save at the end
 print("Saving Final")
 workbook.save(r"C:\Users\Connor\Documents\TextBookWrite.xls")
 print("Saved Final")
